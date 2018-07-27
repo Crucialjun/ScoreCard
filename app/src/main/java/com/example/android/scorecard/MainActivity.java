2753c28ac68eb0daf8dea12b3912bdb079b922c2
Crucialjun <nich.otieno@gmail.com>
2018-07-27 17:19:13 +0300

    Layout Design

--- app/src/main/java/com/example/android/scorecard/MainActivity.java
+++ app/src/main/java/com/example/android/scorecard/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.android.scorecard;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    int teamAGoals = 0;
+    int teamBGoals = 0;
+    int teamACorners = 0;
+    int teamBCorners = 0;
+    int teamAFouls = 0;
+    int teamBFouls = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Goal Team A on Click action
+    public void teamAGoals(View view) {
+        teamAGoals++;
+    }
+
+
 }
